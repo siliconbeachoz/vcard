40a4355cfc1f9a77948a41956d134e35f795bd33
Alexar <hi@alexar.me>
2015-05-03 14:07:45 +1000

    Add a cover photo

---
+++ back.php
@@ -0,0 +1,6 @@
+	<section class="back">
+		<div class="cover">
+			<img src="images/think_differently.gif" />
+			<!-- Copyright: http://www.coolartvinyl.com.au/different-world-wall-sticker-p-531.html -->
+		</div>
+	</section>
