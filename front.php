fd1dfdb04de6dadcc2b5c62d4002d8e0151f0d62
Alexar <hi@alexar.me>
2015-05-03 14:06:37 +1000

    Replacing the website URL with twitter hashtag and handle

--- front.php
+++ front.php
@@ -3,6 +3,7 @@
 			<img src="logo.png" alt="Silicon Beach Logo" />
 		</div>
 		<div class="url">
-			<a href="http://siliconbeachaustralia.org/">http://siliconbeachaustralia.org/</a>
+			<a href="https://twitter.com/hashtag/siliconbeachoz">#SiliconBeachOz</a>
+			<a href="https://twitter.com/SiliconBeachMel/">@SiliconBeachMel</a>
 		</div>
 	</section>
