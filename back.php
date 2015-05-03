0eb7f5e348a1b4a18dd1584c2fa2d38bfe745b13
Alexar <hi@alexar.me>
2015-05-03 14:16:41 +1000

    Add the github repository of the card source code

--- back.php
+++ back.php
@@ -3,4 +3,7 @@
 			<img src="images/think_differently.gif" />
 			<!-- Copyright: http://www.coolartvinyl.com.au/different-world-wall-sticker-p-531.html -->
 		</div>
+		<div class="git">
+			$ git clone git@github.com:siliconbeachoz/vcard.git
+		</div>
 	</section>
