04dce2154cac68af63f5d7200e6981746288b9c5
Alexar <hi@alexar.me>
2015-05-03 13:23:09 +1000

    First version of Silicon Beach business card

---
+++ front.php
@@ -0,0 +1,8 @@
+	<section>
+		<div class="logo">
+			<img src="logo.png" alt="Silicon Beach Logo" />
+		</div>
+		<div class="url">
+			<a href="http://siliconbeachaustralia.org/">http://siliconbeachaustralia.org/</a>
+		</div>
+	</section>
---
+++ full.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<?php include('front.php'); ?>
+	<?php include('back.php'); ?>
+</body>
+</html>
